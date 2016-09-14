4864261f1feb4aa28c94c04523e2a875757f1e7b
Hailey Hurt <hhurt@usc.edu>
2016-09-14 00:02:32 -0700

    Base code project for 2D side scroller

---
+++ NewGalactic/Assets/Scripts/Reset.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reset : MonoBehaviour {
+	public GameObject player;
+	public Vector3 resetPos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ResetPlayer(){
+		player.transform.position = resetPos;
+	}
+}
