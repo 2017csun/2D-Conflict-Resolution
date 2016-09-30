1625f553c7d79f3280ed6fc2d238feab65087a97
Carrie Sun <carriesu@usc.edu>
2016-09-29 22:00:20 -0700

    character selection menu with very basic toggle, animation doesn't work yet

---
+++ NewGalactic/Assets/Scripts/CharacterToggleScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CharacterToggleScript : MonoBehaviour {
+
+    SpriteRenderer sr;
+    // vector to keep track of selected character/character options
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            Debug.Log("enter pressed");
+            SceneManager.LoadScene("Level");
+        }
+	    else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("left arrow pressed");
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("Right arrow pressed");
+            sr = gameObject.GetComponent<SpriteRenderer>();
+            Debug.Log(sr.sprite.texture);
+            sr.sprite = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Sprites/Character/Asteroid.png", typeof(Sprite));
+        }
+	}
+
+    public void OnToggle()
+    {
+        Debug.Log("Character toggled.");
+    }
+}
