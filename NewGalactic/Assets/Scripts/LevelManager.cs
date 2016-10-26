ab7dbbf38ce4add40aa6621264fb59a793b40203
Carrie Sun <carriesu@usc.edu>
2016-10-26 15:19:09 -0700

    UI/character/edge changes (#9)
    * font, exit button prefab, character selection background
    
    * edge colliders and added some more character colors

--- NewGalactic/Assets/Scripts/LevelManager.cs
+++ NewGalactic/Assets/Scripts/LevelManager.cs
@@ -17,8 +17,9 @@ public class LevelManager : Photon.PunBehaviour {
 
 
 	public void ResetGame(){
-		LoadScene ("MainMenu");
-
+		SceneManager.LoadScene ("MainMenu");
+        //TODO: more logic here when multiple players are involved. 
+        // prompt user to confirm, then if they decide to exit, alert the other player.
 	}
 
     [PunRPC]
