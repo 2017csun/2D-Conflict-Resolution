9c79670fdc17e2589bd7a56423e3496cf043cf99
Carrie Sun <carriesu@usc.edu>
2016-10-25 23:31:33 -0700

    font, exit button prefab, character selection background

--- NewGalactic/Assets/Scripts/LevelManager.cs
+++ NewGalactic/Assets/Scripts/LevelManager.cs
@@ -14,8 +14,9 @@ public class LevelManager : MonoBehaviour {
     }
 
 	public void ResetGame(){
-		LoadScene ("MainMenu");
-
+		SceneManager.LoadScene ("MainMenu");
+        //TODO: more logic here when multiple players are involved. 
+        // prompt user to confirm, then if they decide to exit, alert the other player.
 	}
 	
 }
