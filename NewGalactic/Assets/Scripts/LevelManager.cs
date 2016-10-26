1a2554f1af6cd8917deff88e7cbcae15f72e85da
Josh London <joshklondon@gmail.com>
2016-10-26 04:16:04 -0700

    switch to RPC for masterclient levelloading

--- NewGalactic/Assets/Scripts/LevelManager.cs
+++ NewGalactic/Assets/Scripts/LevelManager.cs
@@ -5,18 +5,26 @@ using System.Collections;
 public class LevelManager : Photon.PunBehaviour {
 
 	public void Start(){
-		DontDestroyOnLoad (gameObject);
+		DontDestroyOnLoad (this.gameObject);
         PhotonNetwork.automaticallySyncScene = true;
     }
 
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        this.photonView.RPC("LoadSceneRPC", PhotonTargets.MasterClient, sceneName);
     }
+
+
 
 	public void ResetGame(){
 		LoadScene ("MainMenu");
 
 	}
-	
+
+    [PunRPC]
+    void LoadSceneRPC(string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+
 }
