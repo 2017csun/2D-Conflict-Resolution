6e3d91bb7037fe48fd38c6e5ebbef9243f01fb82
Hailey Hurt <hhurt@usc.edu>
2016-10-28 09:10:57 -0700

    spinnpinners completed in a hacked up version

---
+++ NewGalactic/Assets/Scripts/SlotTriggerSlow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlotTriggerSlow : Photon.PunBehaviour {
+
+	public Animator slotAnimator;
+	public bool isConflict = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		if (!isConflict) {
+			if (collider.gameObject.GetComponent<PlayerControl> ().thisOneIsLocal) {
+				Debug.Log ("Setting trigger");
+				slotAnimator.SetTrigger ("SlowToAccommodating");
+			}
+		} else {
+			if (collider.gameObject.CompareTag ("Player")) {
+				Debug.Log ("Setting trigger");
+				slotAnimator.SetTrigger ("SlowToAccommodating");
+			}
+		}
+	}
+}
