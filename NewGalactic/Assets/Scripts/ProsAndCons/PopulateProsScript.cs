bb4bd5218f64785aa1b47dbfe9b877157a8627ec
Carrie Sun <carriesu@usc.edu>
2016-10-31 12:29:10 -0700

    finished populating pros and cons scripts; hardcoded intention

---
+++ NewGalactic/Assets/Scripts/ProsAndCons/PopulateProsScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class PopulateProsScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+
+        string[] wrongAnswers = this.getWrongAnswers();
+        string[] correctAnswers = this.getCorrectAnswers();
+        ProsAndConsHelper.populateButtons(gameObject, correctAnswers, wrongAnswers);
+
+    }
+    // add logic for intentions here
+    private string[] getWrongAnswers()
+    {
+        return new string[]
+        {
+            "Asserting your position so ideas are taken seriously",
+            "Making quick decisions or achieving quick victory",
+            "Protecting your interests and views from attack",
+            "Working toward meeting both people’s concern",
+            "Resolving problems in a relationship",
+            "Getting to a situation that is good enough",
+            "Seeking innovative solutions and creating synergy through the exchange of ideas",
+            "Meeting half way to reduce relationship strain",
+            "Reducing stress by evading unpleasant people and topics",
+            "Gaining more time to be better prepared and be less distracted",
+            "Not stirring up problems or provoking trouble",
+        };
+    }
+    private string[] getCorrectAnswers()
+    {
+        return new string[]{
+            "Helping people meet their needs",
+            "Supporting people and calming people down",
+            "Building social capital by doing favors",
+        };
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ NewGalactic/Assets/Scripts/ProsAndCons/ProsAndConsHelper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+    class ProsAndConsHelper : MonoBehaviour
+    {
+        static public void populateButtons(
+            GameObject gameObject,
+            string[] correctAnswers,
+            string[] wrongAnswers
+            )
+        {
+
+        bool[] indeces = generateIndeces();
+        Button[] children = gameObject.GetComponentsInChildren<Button>();
+        int buttonCounter = 0;
+        int trueCounter = 0;
+
+        System.Random random = new System.Random();
+        foreach (Button button in children)
+        {
+            if (indeces[buttonCounter] == true && trueCounter < 3)
+            {
+                button.GetComponentInChildren<Text>().text = correctAnswers[trueCounter];
+                trueCounter++;
+            }
+            else
+            {
+                int index = (int)random.Next(0, wrongAnswers.Length - 1);
+                button.GetComponentInChildren<Text>().text = wrongAnswers[index];
+            }
+            buttonCounter++;
+        }
+    }
+        static public bool[] generateIndeces()
+        {
+            bool[] indeces = new bool[6];
+            System.Random random = new System.Random();
+            int numTrue = 0;
+            //PSEUDO RANDOM PRO/CON POSITIONING GENERAtION
+            for (int i = 0; i < 6; i++)
+            {
+                int rnd = random.Next(0, 4);
+                if (rnd > 1)
+                {
+                    indeces[i] = false;
+                }
+                else
+                {
+                    indeces[i] = true;
+                    numTrue++;
+                }
+            }
+            if (numTrue != 3)
+            {
+                for (int i = 5; i >= 0; i--)
+                {
+                    if (indeces[i] == false)
+                    {
+                        indeces[i] = true;
+                        numTrue++;
+                        if (numTrue == 3)
+                        {
+                            i = -1;
+                        }
+                    }
+                }
+            }
+            return indeces;
+        }
+    }
